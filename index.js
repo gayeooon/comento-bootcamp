fb18aa359a3085622de158dd65d83fbc4a5dae2d
Gayeon Lee <gayean430@gmail.com>
2024-01-12 23:01:51 +0900

    final

--- index.js
+++ index.js
@@ -8,74 +8,63 @@ const opBtns = document.querySelectorAll(".cal__btns__op");
 const inputHistory = [];
 const resultHistory = [];
 
+// 숫자 버튼 구현
+const numClink = function (btn) {
+  if (result.textContent !== "") {
+    result.textContent = "";
+    prevInput.textContent = "";
+  }
+  if (input.textContent.length >= 14) {
+    alert("14자리까지 입력할 수 있습니다.");
+  } else {
+    input.textContent += btn.textContent;
+  }
+};
+
 numberBtns.forEach(function (btn) {
   btn.addEventListener("click", function () {
-    if (result.textContent !== "") {
-      result.textContent = "";
-      prevInput.textContent = "";
-    }
-    input.textContent += btn.textContent;
+    numClink(btn);
   });
 });
 
 // operator 버튼 구현
 let opOrder = false;
 
+const opClick = function () {
+  if (input.textContent !== "") {
+    let number = parseInt(input.textContent);
+    prevInput.textContent += number;
+    input.textContent = "";
+
+    prevInput.scrollTo(0, prevInput.scrollHeight);
+    opOrder = true;
+  }
+};
+
 opBtns.forEach(function (btn) {
-  btn.addEventListener("click", function () {
-    if (input.textContent !== "") {
-      let number = parseInt(input.textContent);
-      prevInput.textContent += number;
-      input.textContent = "";
-      opOrder = true;
+  btn.addEventListener("click", opClick);
+});
+
+// operator 추가
+const addOperator = function (id, operator) {
+  const opBtn = document.getElementById(id);
+
+  opBtn.addEventListener("click", function () {
+    if (opOrder) {
+      prevInput.textContent += operator;
+      opOrder = false;
     }
   });
-});
+};
 
-// plus 버튼 구현
-const plus = document.getElementById("cal__btns__plus");
-
-plus.addEventListener("click", function () {
-  if (opOrder) {
-    prevInput.textContent += "+";
-    opOrder = false;
-  }
-});
-
-// minus 버튼 구현
-const minus = document.getElementById("cal__btns__minus");
-
-minus.addEventListener("click", function () {
-  if (opOrder) {
-    prevInput.textContent += "-";
-    opOrder = false;
-  }
-});
-
-// multiply 버튼 구현
-const multiply = document.getElementById("cal__btns__multiply");
-
-multiply.addEventListener("click", function () {
-  if (opOrder) {
-    prevInput.textContent += "*";
-    opOrder = false;
-  }
-});
-
-// divide 버튼 구현
-const divide = document.getElementById("cal__btns__divide");
-
-divide.addEventListener("click", function () {
-  if (opOrder) {
-    prevInput.textContent += "/";
-    opOrder = false;
-  }
-});
+addOperator("cal__btns__plus", "+");
+addOperator("cal__btns__minus", "-");
+addOperator("cal__btns__multiply", "*");
+addOperator("cal__btns__divide", "/");
 
 // equal 버튼 구현
-const equal = document.getElementById("cal__btns__equals");
 
-equal.addEventListener("click", function () {
+const equalClick = function () {
   if (opOrder) {
     let resultNum = parseFloat(eval(prevInput.textContent));
     resultNum = parseFloat(resultNum.toFixed(5));
@@ -84,28 +73,31 @@ equal.addEventListener("click", function () {
     inputHistory.push(prevInput.textContent);
     opOrder = false;
   }
-});
+};
+
+const equal = document.getElementById("cal__btns__equals");
+equal.addEventListener("click", equalClick);
 
 // reset 버튼 구현
-const reset = document.getElementById("cal__btns__reset");
-
-reset.addEventListener("click", function () {
+const resetClick = function () {
   prevInput.textContent = "";
   input.textContent = "";
   result.textContent = "";
-});
+};
+
+const reset = document.getElementById("cal__btns__reset");
+reset.addEventListener("click", resetClick);
 
 // history 버튼 구현
 const history = document.getElementById("cal__history");
 const screen = document.getElementById("cal__screen");
-let historyOn = false;
 
-history.addEventListener("click", function () {
+const historyClick = function () {
   let screenDiv = document.getElementById("cal__screen__history");
 
-  if (historyOn) {
+  if (screenDiv) {
     screenDiv.remove();
-    historyOn = false;
+    history.style.backgroundColor = "";
   } else {
     let newDiv = document.createElement("div");
     newDiv.id = "cal__screen__history";
@@ -113,11 +105,13 @@ history.addEventListener("click", function () {
 
     screenDiv = document.getElementById("cal__screen__history");
 
-    for (let i = 0; i < inputHistory.length; i++) {
+    for (let i = inputHistory.length - 1; i >= 0; i--) {
       screenDiv.innerHTML +=
-        inputHistory[i] + " = " + resultHistory[i] + "<br>";
+        inputHistory[i] + " = " + resultHistory[i] + "<br /><br />";
     }
 
-    historyOn = true;
+    history.style.backgroundColor = "rgb(197, 214, 198)";
   }
-});
+};
+
+history.addEventListener("click", historyClick);
